c79f262d20272aa474ba832bee2be26765d4b384
Jayson Jiang <suisiyuan615@qq.com>
2016-10-15 17:12:57 +0800

    将项目代码改成qmake工程形式，能够通过脚本生成vs工程文件。

---
+++ setup/generate_vs_project.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+echo Setting up a Qt environment...
+
+set QMAKESPEC=win32-msvc2015
+echo -- QMAKESPEC set to "win32-msvc2015"
+
+set QTDIR=C:\Qt\Qt5.7.0\5.7\msvc2015
+echo -- QTDIR set to %QTDIR%
+
+set PATH=%QTDIR%\bin;%PATH%
+echo -- Added %QTDIR%\bin to PATH
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+echo --generate DCTA VS project
+cd ..
+qmake -t vcapp

e9155f674e2fde3b4abf03232833c22df1421e15
Jayson Jiang <suisiyuan615@qq.com>
2016-10-26 21:49:33 +0800

    简单修改app.h。

--- src/app.h
+++ src/app.h
@@ -1,12 +1,11 @@
 #ifndef APP_H
 #define APP_H
 
-// define application
+
 #define COMPANY_NAME    "ylink"
 #define COMPANY_URL     "http://www.y-link.cn/"
 #define APP_NAME        "IDT_CAPTURE"
 #define APP_VERSION     "1.0.0"
-
 
 
 #define DEFAULT_FOLDER  (QString("C:/") + COMPANY_NAME)
